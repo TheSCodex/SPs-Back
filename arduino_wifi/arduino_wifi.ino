be5bc6ebe482a8a02eaed327e17abc7dd2073288
TheSCodex <eliud1102@outlook.es>
2024-04-07 16:20:00 -0500

    Arduino consumidor de endpoints

--- arduino_wifi/arduino_wifi.ino
+++ arduino_wifi/arduino_wifi.ino
@@ -1,5 +1,6 @@
 #include <WiFiS3.h>
 #include <PubSubClient.h>
+#include <ArduinoHttpClient.h>
 #define NUM_SENSORS 1
 
 const int sensorPin[NUM_SENSORS] = {2};
@@ -8,13 +9,16 @@ const int greenPin[NUM_SENSORS] = {6};
 
 const char* ssid = "Totalplay-5DAA";
 const char* password = "5DAAD5479GTARA2X"; 
-const char* mqtt_server = "192.168.100.94";
+const char* mqtt_server = "192.168.100.32";
+const char* serverAddress = "192.168.100.32";
+int serverPort = 8080;
 
-WiFiClient espClient;
+WiFiClient wifi;
+HttpClient httpClient = HttpClient(wifi, serverAddress, serverPort);
 PubSubClient client(espClient);
 
 unsigned long lastStateChangeTime[NUM_SENSORS] = {0};
-int lastState[NUM_SENSORS] = {-1}; // Initialize to an invalid state
+int lastState[NUM_SENSORS] = {-1};
 
 void setup() {
   Serial.begin(9600);
@@ -60,11 +64,13 @@ void loop(){
         Serial.println("Espacio Ocupado");  //zona oscura
         digitalWrite(redPin[i], HIGH);
         digitalWrite(greenPin[i], LOW);
+        updateParkingSpotStatus(i+1, "occupied");
       } else {
         // Si el sensor no está tapado
         digitalWrite(greenPin[i], HIGH);
         digitalWrite(redPin[i], LOW);
         Serial.println("Espacio Libre");  //zona libre
+        updateParkingSpotStatus(i+1, "unoccupied");
       }
       client.publish("arduino/sensor", message);
     }
@@ -83,5 +89,24 @@ void reconnect() {
       Serial.println(" try again in 5 seconds");
       delay(5000);
     }
+  }
+}
+
+void updateParkingSpotStatus(int spotId, const char* status) {
+  if(WiFi.status()== WL_CONNECTED){
+    String path = "/status/" + String(status) + "/" + String(spotId);
+
+    httpClient.get(path);
+
+    int statusCode = httpClient.responseStatusCode();
+    String response = httpClient.responseBody();
+
+    Serial.print("Status code: ");
+    Serial.println(statusCode);
+    Serial.print("Response: ");
+    Serial.println(response);
+  }
+  else{
+    Serial.println("WiFi Disconnected");
   }
 }
