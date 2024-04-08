6924c7b860bf076ab216646e641be1ce5b291d46
ppeguero <frdpgr@gmail.com>
2024-04-08 10:47:38 -0500

    Se añadieron las rutas de reservaciones

--- src/routes/arduinoRoutes.js
+++ src/routes/arduinoRoutes.js
@@ -6,5 +6,6 @@ const arduinoRoutes = express.Router();
 arduinoRoutes.get('/status/occupied/:id', arduinoControllers.changeStatusToOccupied);
 arduinoRoutes.get('/status/unoccupied/:id', arduinoControllers.changeStatusToUnoccupied);
 arduinoRoutes.get('/status/parkingSpots', arduinoControllers.getParkingStatus);
+arduinoRoutes.get('/status/availableParkingSpots', arduinoControllers.getAvailableParkingSpots);
 
 export default arduinoRoutes;
